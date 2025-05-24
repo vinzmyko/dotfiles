d062ec9adc416d73131b53b385df6abbe4621c85
Vinz Myko Del Rosario <vinzmykodelrosario@gmail.com>
2025-05-24 19:46:40 +0100

    refactor: fix > creation/movement and ' creation for rust

--- nvim/.config/nvim/lua/plugins/nvim-autopairs.lua
+++ nvim/.config/nvim/lua/plugins/nvim-autopairs.lua
@@ -5,14 +5,17 @@ return {
     config = function()
         local npairs = require("nvim-autopairs")
         local Rule = require("nvim-autopairs.rule")
+        local cond = require("nvim-autopairs.conds")
 
         npairs.setup({
             check_ts = true,
             disable_filetype = { "TelescopePrompt" },
-            -- Make this very permissive
-            ignored_next_char = ".",
             enable_bracket_in_quote = true,
             enable_check_bracket_line = false,
+            -- This is important for angle brackets - it allows moving through them
+            enable_moveright = true,
+            -- This helps with angle bracket detection
+            ignored_next_char = [=[[%w%%%'%[%"%.%`%$]]=],
         })
 
         -- Integration with nvim-cmp
@@ -22,23 +25,29 @@ return {
             cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
         end
 
-        -- Remove any existing angle bracket rule
+        -- Remove single quote rule for Rust files
+        -- This prevents auto-pairing of single quotes which interfere with lifetime syntax
+        npairs.remove_rule("'")
+        npairs.add_rule(Rule("'", "'"):with_pair(cond.not_filetypes({ "rust" })))
+
+        -- Better angle bracket handling for Rust
+        -- First remove any existing angle bracket rules
         npairs.remove_rule("<")
 
-        -- Add a simpler, more permissive rule for angle brackets in Rust
-        npairs.add_rules({
-            -- Simple angle bracket rule with no conditions
-            Rule("<", ">", { "rust" }),
-        })
-
-        -- Override the nested parentheses rule
-        local rules = npairs.get_rules("(")
-        rules[1]
-            :with_pair(function()
-                return true
-            end)
+        -- Add a smarter rule for angle brackets in Rust
+        npairs.add_rule(Rule("<", ">", "rust")
+            -- Only add pair when preceded by a word character (for generics)
+            :with_pair(cond.before_regex("%w"))
+            -- Allow moving through the closing bracket
             :with_move(function(opts)
-                return opts.char == ")"
+                return opts.char == ">"
             end)
+            -- Don't pair when followed by =, -, or > (for operators like <=, <-, <<)
+            :with_pair(
+                cond.not_after_regex("[%-%=>]")
+            ))
+
+        -- Optional: Add rules for other filetypes that might need angle brackets
+        npairs.add_rule(Rule("<", ">", { "html", "xml", "typescriptreact", "javascriptreact" }))
     end,
 }
