49c3fbf55e22e2284a6445e5c909f3e7f6dc607f
Vinz Myko Del Rosario <vinzmykodelrosario@gmail.com>
2025-06-18 17:23:06 +0100

    refactor: remove mini.indentscope for cleaner indentation guides

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -132,40 +132,6 @@ return {
         end,
     },
 
-    -- Indentscope (shows current scope)
-    {
-        "echasnovski/mini.indentscope",
-        event = "VeryLazy",
-        opts = {
-            symbol = "│",
-            options = { try_as_border = true },
-            draw = {
-                delay = 0,                           -- No delay
-                animation = function() return 0 end, -- No animation
-            },
-        },
-        init = function()
-            vim.api.nvim_create_autocmd("FileType", {
-                pattern = {
-                    "help",
-                    "alpha",
-                    "dashboard",
-                    "neo-tree",
-                    "Trouble",
-                    "trouble",
-                    "lazy",
-                    "mason",
-                    "notify",
-                    "toggleterm",
-                    "lazyterm",
-                },
-                callback = function()
-                    vim.b.miniindentscope_disable = true
-                end,
-            })
-        end,
-    },
-
     -- Move lines/blocks (alternative to your Primeagen mappings)
     {
         "echasnovski/mini.move",
